83f87ce8d1471197937bc9f391b9c10caaf4200e
HeyKJ <iamheykj@gmail.com>
2016-07-08 13:59:36 +0900

    잇힝

--- app/src/main/java/com/example/adsw21/firstandroid/MainActivity.java
+++ app/src/main/java/com/example/adsw21/firstandroid/MainActivity.java
@@ -5,9 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button)findViewById(R.id.loginBtn);
         btn.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
                 startActivity(intent);
             }
         });
